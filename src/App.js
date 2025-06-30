28019026f017c83dfa095d89038ad99793a9a84b
Liam Shalom <lsdws@MacBook-Air.local>
2025-06-30 11:59:16 -0700

    beginner nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
+
+
+  
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
       Fuck you Solden
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
+
+    const [navVisible, setNavVisible] = useState(true);
+    const navigate = useNavigate();
+
+    const toggleNav = () => {
+        setNavVisible(!navVisible);
+    }
+
+
     return (
-        <header>
-            <nav>
-                Header
-            </nav>
+        <header className="header">
+            {navVisible && (
+                <nav className="nav">
+                    <a onClick={() => navigate('/about')}>About</a>
+                    <a href="/">Rush</a>
+                    <a href="/">Exec</a>
+                    <a href="/">Gallery</a>
+                </nav>
+            )}
+            <button className="nav-button" onClick={toggleNav}>0</button>
         </header>
     )
 }
